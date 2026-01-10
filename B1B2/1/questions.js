5fa47e32622b8847be82e3097b58b947d956a0ae
PapeleraCotillonGamar <joseroquegaspar@gmail.com>
2026-01-10 13:30:23 -0300

    Add files via upload

---
+++ B1B2/1/questions.js
@@ -0,0 +1,76 @@
+const questions = [
+
+
+{
+        question: "She ___ a video when the phone rang.",
+        answers: ["edits", "was editing", "has edited", "will edit"],
+        correct: 1
+    },
+    {
+        question: "By next week, we ___ the project.",
+        answers: ["finish", "finished", "will have finished", "are finishing"],
+        correct: 2
+    },
+    {
+        question: "The video ___ yesterday.",
+        answers: ["is edited", "was edited", "has edited", "edited"],
+        correct: 1
+    },
+    {
+        question: "The audio ___ by the editor right now.",
+        answers: ["is being mixed", "was mixed", "mixes", "has mixed"],
+        correct: 0
+    },
+    {
+        question: "He said: 'I am exporting the video.'",
+        answers: [
+            "He said he is exporting the video.",
+            "He said he was exporting the video.",
+            "He said he exports the video.",
+            "He said he had exported the video."
+        ],
+        correct: 1
+    },
+    {
+        question: "She said: 'We finished the edit.'",
+        answers: [
+            "She said they finished the edit.",
+            "She said they had finished the edit.",
+            "She said they finish the edit.",
+            "She said they were finishing the edit."
+        ],
+        correct: 1
+    },
+    {
+        question: "If I ___ more time, I would learn color grading.",
+        answers: ["have", "had", "will have", "has"],
+        correct: 1
+    },
+    {
+        question: "If you export in 4K, the file ___ bigger.",
+        answers: ["is", "was", "will be", "would be"],
+        correct: 2
+    },
+    {
+        question: "The footage had already ___ when we arrived.",
+        answers: ["edit", "editing", "edited", "been edited"],
+        correct: 3
+    },
+    {
+        question: "He asked me if I ___ the timeline.",
+        answers: ["finish", "finished", "had finished", "have finished"],
+        correct: 2
+    },
+    {
+        question: "This effect ___ in most professional videos.",
+        answers: ["uses", "is used", "used", "was using"],
+        correct: 1
+    },
+    {
+        question: "She told me not ___ the original file.",
+        answers: ["delete", "to delete", "deleting", "deleted"],
+        correct: 1
+    }
+
+
+];
